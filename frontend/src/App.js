e502962a69c81f37ced931dd2578cc027bf94c4f
shivanitiwari83 <shivanitiwari09112001@gmail.com>
2022-11-15 14:00:21 +0530

    ..

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter, Route, Link, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Link, Routes, Navigate } from 'react-router-dom';
 import Main from './components/main';
 import Login from './components/main/Login';
 import Signup from './components/main/Signup';
@@ -8,13 +8,16 @@ import Admin from './components/admin';
 import User from './components/user';
 import AdminProfile from './components/admin/Profile';
 import UserProfile from './components/user/Profile';
+import Header from './components/main/Header';
 
 function App() {
   return (
     <div>
       <BrowserRouter>
+      <Header />
         <Routes>
 
+          <Route element={<Navigate to="/main/login" />} path="/" />
           <Route element={<Main />} path="main">
             <Route path="login" element={<Login />} />
             <Route path="signup" element={<Signup />} />
--- frontend/src/components/main/Header.js
+++ frontend/src/components/main/Header.js
@@ -1,11 +1,116 @@
-import React from 'react'
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
 
 const Header = () => {
+  const [loggedin, setLoggedin] = useState(false);
+
   return (
-    <div>
-        <h1>Main Header</h1>
-    </div>
-  )
-}
+    <>
+      {/* Navbar */}
+      <nav
+        className="navbar navbar-expand-lg navbar-dark"
+        style={{ backgroundColor: "#232a69" }}
+      >
+        {/* Container wrapper */}
+        <div>
+          {/* Toggle button */}
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-mdb-toggle="collapse"
+            data-mdb-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <i className="fas fa-bars" />
+          </button>
+          {/* Collapsible wrapper     */}
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              {/* Link */}
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/home">
+                  Home
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/login">
+                  Login
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/signup">
+                  Sign Up
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/manageAsset">
+                  Manage Assets
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/userManager">
+                  User Manager
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/contactInfo">
+                  Contact Information
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/addForm">
+                  Add Form
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/listForm">
+                  List Form
+                </NavLink>
+              </li>
+              
+            </ul>
+            {/* Icons */}
+            <ul className="navbar-nav d-flex flex-row me-1">
+              <li className="nav-item me-3 me-lg-0">
+                {loggedin ? (
+                  <button
+                    className="btn btn-danger"
+                    onClick={() => {
+                      setLoggedin(false);
+                    }}
+                  >
+                    <i className="fas fa-sign-out-alt"></i>Logout
+                  </button>
+                ) : (
+                  <button
+                    className="btn btn-primary"
+                    onClick={() => {
+                      setLoggedin(true);
+                    }}
+                  >
+                    <i className="fas fa-sign-in"></i> Login
+                  </button>
+                )}
+              </li>
+            </ul>
+            {/* Search */}
+            <form className="w-auto">
+              <input
+                type="search"
+                className="form-control"
+                placeholder="Type query"
+                aria-label="Search"
+              />
+            </form>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+
 
 export default Header;
