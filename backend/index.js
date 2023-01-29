7d49a8ad2621bb74bc64239185f8facb36fcbd17
m-triple-m <triplem656@mail.com>
2023-01-29 14:59:50 +0530

    ..

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ const stripe = require("stripe")(stripe_sk);
 const cors = require("cors");
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(
--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,6 @@
 const app_config = {
-    api_url : 'http://localhost:5000'
+    // api_url : 'http://localhost:5000',
+    api_url : 'https://shootit-backend.onrender.com',
 };
 
 export default app_config;
